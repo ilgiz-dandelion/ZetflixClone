f97c79a3502f7e28512c89b0f10b27a0806bd4d1
Your Name <you@example.com>
2021-11-25 15:42:05 +0600

    models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from main.models import *
+
+
+class ImageInLine(admin.TabularInline):
+    model = Image
+    max_num = 6
+    min_num = 1
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    inlines = [ImageInLine, ]
+
+
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Likes)
+admin.site.register(Review)
+admin.site.register(Reply)
+
